966e8ae536ede0a10968fe250898336945954c80
daanhaa <eksgk0401@gmail.com>
2025-04-04 00:11:09 +0900

    Update: code1.cpp

---
+++ code1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Programming is fun\n";
+    
+
+    int age = 25;
+    double height = 170.5;
+    bool is_student = true;
+    cout<< "age is " << age <<"\n";
+
+    char a = 'C';
+    a = 'T';
+    printf("%c", a);
+
+    return 0;
+}
+
